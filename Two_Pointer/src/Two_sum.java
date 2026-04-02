f9b500787db63ca8ca5467bd8abd27113df7b6b2
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-04-02 17:05:28 +0530

    Two sum return value/index (value in sorted order)

---
+++ Two_Pointer/src/Two_sum.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Two_sum {
+    public static void main(String[] args) {
+
+        // Find two number in sorted array
+
+        int arr[] = {2, 3, 5, 7, 9};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(arr, target)));
+    }
+
+    public static int[] twoSum(int[] arr, int target) {
+
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+            int sum = arr[i] + arr[j];
+            if (sum == target) {
+                return new int[]{arr[i], arr[j]};
+            } else if (target > sum) {
+                i++;
+            }
+            j--;
+        }
+        return new int[]{-1, -1};
+    }
+}
