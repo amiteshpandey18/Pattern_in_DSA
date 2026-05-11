b4bf0ff13cc0269bec1f681ccd8fe33eee01ed2e
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-05-11 15:42:16 +0530

    Reserve String non non-alphanumeric characters

--- Two_Pointer/src/ReverseString.java
+++ Two_Pointer/src/ReverseString.java
@@ -4,21 +4,47 @@ public class ReverseString {
 
     public static void main(String[] args) {
 
-        String str = "Rohit";
-        System.out.println("Before reverse: " + str);
+//        String str = "Rohit";
+//        System.out.println("Before reverse: " + str);
+//
+//        char[] ch = str.toCharArray();
+//        int i = 0, j = str.length() - 1;
+//
+//        while (i < j) {
+//            char temp = ch[i];
+//            ch[i] = ch[j];
+//            ch[j] = temp;
+//            i++;
+//            j--;
+//        }
+//
+//        str = new String(ch);
+//        System.out.println("After Reverse: " + str);
 
-        char[] ch = str.toCharArray();
-        int i = 0, j = str.length() - 1;
 
-        while (i < j) {
-            char temp = ch[i];
-            ch[i] = ch[j];
-            ch[j] = temp;
-            i++;
-            j--;
+        // Optimize way
+
+        String str = "A man, a plan, a canal: Panama";
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(str.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(str.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(str.charAt(left)) != Character.toLowerCase(str.charAt(right))) {
+                System.out.println("Not palindrome");
+                return;
+            }
+            left++;
+            right--;
         }
-
-        str = new String(ch);
-        System.out.println("After Reverse: " + str);
+        System.out.println("palindrome");
     }
 }
